d40c81af3b20f2f0c512c549a08b0dd6816ac29f
rindid <48424638+rindid@users.noreply.github.com>
2021-02-15 21:57:26 +0900

    init

---
+++ README.md
@@ -0,0 +1,32 @@
+# img_cropper
+
+## USED
+
+* python 3.9.1
+* numpy 1.20.1
+* opencv-python 4.5.1.48
+
+
+
+## 목표
+
+* ocr학습 및 인식을 위해 이미지를 적절한 ROI(Region of Interest)로 자르는 것.
+
+
+
+## 제반사항
+
+* 주어진 이미지는 노이즈가 없는 이미지를 가정한다. (이미지화 된 문서 파일 혹은 이미지화 된 pdf파일 등, 주로 사무자동화에 사용될 이미지를 가정하였다.)
+* ROI는 텍스트와 로고(작은 이미지)이다.
+
+
+
+## 참고자료
+
+* 기반 코드(https://stackoverflow.com/questions/23506105/extracting-text-opencv/23565051#23565051)
+* 위 코드를 찾은 포스팅(https://blog.naver.com/PostView.nhn?blogId=monkey5255&logNo=221598376164&redirect=Dlog&widgetTypeCall=true&directAccess=false)
+* 코드의 전반적인 흐름 및 사용 기술을 파악하기에 좋은 자료(https://d2.naver.com/helloworld/8344782)
+* 기타 블로그 및 포스팅등을 활용하여 기반 기술(opencv, numpy, python)을 이해하였다.
+
+
+
---
+++ img_cropper.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+import os
+
+path = os.getcwd()
+large = cv2.imread(path+'\image\\test.png')
+rgb = cv2.pyrDown(large)
+small = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+grad = cv2.morphologyEx(small, cv2.MORPH_GRADIENT, kernel)
+_, bw = cv2.threshold(grad, 0.0, 255.0, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 1))
+connected = cv2.morphologyEx(bw, cv2.MORPH_CLOSE, kernel)
+# using RETR_EXTERNAL instead of RETR_CCOMP
+contours, hierarchy = cv2.findContours(connected.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+mask = np.zeros(bw.shape, dtype=np.uint8)
+for idx in range(len(contours)):
+    x, y, w, h = cv2.boundingRect(contours[idx])
+    mask[y:y+h, x:x+w] = 0
+    cv2.drawContours(mask, contours, idx, (255, 255, 255), -1)
+    r = float(cv2.countNonZero(mask[y:y+h, x:x+w])) / (w * h)
+    if r > 0.45 and w > 8 and h > 8:
+        cv2.rectangle(rgb, (x, y), (x+w-1, y+h-1), (0, 255, 0), 2)
+# show image with contours rect
+cv2.imshow('rects', rgb)
+cv2.waitKey()
